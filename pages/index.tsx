cb9e96c1bcd49cc233d53a59caabfe05b713b05c
KifKaf-cakie <1207238695@qq.com>
2025-04-05 12:09:19 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
-import EnhancedNudeFiLanding from '../components/EnhancedNudeFiLanding';
-
-export default function Home() {
-  return <EnhancedNudeFiLanding />;
+export default function Test() {
+  return (
+    <div className="min-h-screen bg-gradient-to-r from-pink-500 to-purple-600 text-white flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Hello Tailwind</h1>
+    </div>
+  );
 }
