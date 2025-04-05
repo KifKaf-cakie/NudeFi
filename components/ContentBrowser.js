5a3dd49cccf8442d93eccf79895c20e985edc3a7
KifKaf-cakie <1207238695@qq.com>
2025-04-05 23:15:10 +0900

    Update ContentBrowser.js

--- components/ContentBrowser.js
+++ components/ContentBrowser.js
@@ -75,6 +75,15 @@ const contentItems = [
 
   return (
     <div className="min-h-screen bg-gradient-to-b from-gray-900 via-purple-900/20 to-gray-900 text-white px-4 py-10">
+        <div className="container mx-auto mb-6">
+      <Link href="/" className="flex items-center text-pink-400 hover:text-pink-600">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
+          <path fillRule="evenodd" d="M9.707 16.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l4.293 4.293a1 1 0 010 1.414z" clipRule="evenodd" />
+        </svg>
+        Back to Home
+      </Link>
+    </div>
+          
       <div className="flex justify-between items-center mb-6">
         <div className="flex space-x-4">
           <button
